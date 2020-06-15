4bfe04c831dcd8e7514084a7b3bc18d6e1b255a4
PierreWCS <56911009+PierreWCS@users.noreply.github.com>
2020-06-15 18:16:57 +0200

    removing react router dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./components/Homepage/Homepage";
-import { Switch, Route } from "react-router-dom";
-import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" exact component={Homepage} />
-        <Route path="/about" component={About} />
-      </Switch>
+      <Homepage />
     </div>
   );
 }
