cfcf3c448efb70d9ba52b6056da617cea6493e1a
PierreWCS <56911009+PierreWCS@users.noreply.github.com>
2020-06-29 16:27:42 +0200

    full screen project image feature

--- src/components/ProjectDetails/Carousel.js
+++ src/components/ProjectDetails/Carousel.js
@@ -2,17 +2,30 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "./ProjectDetails.css";
 
-export default ({ imagesData }) => (
-  <Carousel infiniteLoop={true} autoPlay showThumbs={false}>
+export default ({
+  imagesData,
+  setFullScreen,
+  setAutoPlayStatus,
+  autoPlayStatus,
+  setSelectedImage,
+}) => (
+  <Carousel infiniteLoop={true} autoPlay={autoPlayStatus} showThumbs={false}>
     {imagesData
       ? imagesData.images.map((projectImage, key) => {
           return (
-            <div key={key}>
+            <div
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                setAutoPlayStatus(false);
+                setFullScreen(true);
+                setSelectedImage(projectImage);
+              }}
+              key={key}
+            >
               <img
                 src={require(`../../assets/images/${projectImage.image}`)}
                 alt=""
               />
-              <p className="legend">{projectImage.legend}</p>
             </div>
           );
         })
--- src/components/ProjectDetails/ProjectDetails.js
+++ src/components/ProjectDetails/ProjectDetails.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProjectDetails.css";
 import Carousel from "./Carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 
 const ProjectDetails = ({ setProjectDetails, imagesData }) => {
+  const [fullScreen, setFullScreen] = useState(false);
+  const [autoPlayStatus, setAutoPlayStatus] = useState(true);
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
     <div className="projectDetailsContainer">
+      {fullScreen ? (
+        <div className="imageFullSizeContainer">
+          <img
+            className="projectImageFullScreen"
+            src={require(`../../assets/images/${selectedImage.image}`)}
+            alt="project full screen"
+          />
+
+          <FontAwesomeIcon
+            className="closeImageFullScreen fa-3x"
+            onClick={() => {
+              setFullScreen(false);
+              setAutoPlayStatus(true);
+              setSelectedImage(null);
+            }}
+            color="#fff"
+            icon={faTimesCircle}
+          />
+        </div>
+      ) : null}
       <button
         className="closeDetailsButton"
         onClick={() => {
@@ -16,7 +42,15 @@ const ProjectDetails = ({ setProjectDetails, imagesData }) => {
         Close
       </button>
       <div className="firstColDetails">
-        <Carousel imagesData={imagesData} />
+        <Carousel
+          selectedImage={selectedImage}
+          setSelectedImage={setSelectedImage}
+          fullScreen={fullScreen}
+          setFullScreen={setFullScreen}
+          autoPlayStatus={autoPlayStatus}
+          setAutoPlayStatus={setAutoPlayStatus}
+          imagesData={imagesData}
+        />
       </div>
       <div className="secondColDetails">
         <h2>{imagesData.name}.</h2>
@@ -24,22 +58,24 @@ const ProjectDetails = ({ setProjectDetails, imagesData }) => {
         <p>{imagesData.description}</p>
         <h3>Technologies.</h3>
         <p>{imagesData.technologies}</p>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={imagesData.link}
-          className="githubLinkDetails"
-        >
-          See on Github
-        </a>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={imagesData.demo}
-          className="githubLinkDetails"
-        >
-          Try the demo
-        </a>
+        <div className="buttonsContainerProjectDetails">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={imagesData.link}
+            className="githubLinkDetails"
+          >
+            See code
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={imagesData.demo}
+            className="githubLinkDetails"
+          >
+            Try the demo
+          </a>
+        </div>
       </div>
     </div>
   );
